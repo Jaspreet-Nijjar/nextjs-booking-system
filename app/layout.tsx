c254da98d8f0372f697bdf1e217beb0acff391b8
Jaspreet <jaspreetkn92@gmail.com>
2024-11-14 10:22:30 +0000

    created the header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../assets/styles/globals.css';
+import Header from '@/components/Header';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -18,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
